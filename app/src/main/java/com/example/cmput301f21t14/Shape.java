136c8a9780a94d539482959fcb039e99bfc79619
Guosongkun123 <91714571+Guosongkun123@users.noreply.github.com>
2021-10-03 21:54:37 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmput301f21t14/Shape.java
+++ app/src/main/java/com/example/cmput301f21t14/Shape.java
@@ -1,7 +1,7 @@
-package com.example.cmput301f21t14;
+opackage com.example.cmput301f21t14;
 
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "$yellow";
 }
